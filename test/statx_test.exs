1b29ed71281d17d2f762dfce2ccf68d5ca553418
Aziz Shamim <azizshamim@gmail.com>
2015-02-28 20:38:19 +0900

    remove the server start from tests

--- test/statx_test.exs
+++ test/statx_test.exs
@@ -1,23 +1,19 @@
-defmodule Statx.ServerTest do
+defmodule StatxTest do
   use ExUnit.Case
 
-  test "should start a udp socket" do
-  #    # VROOM. Start your engines
-  #    {:ok , server} = Statx.Server.start_link(1514)
+  test "should start the StatX server" do
       {:ok, client_socket} = :gen_udp.open(1515)
-  #
-  #    # Launch missile!
+      # Launch missile!
       :ok = :gen_udp.send(client_socket, {127,0,0,1}, 1514, "BOOM!")
-  #
+
+      # FAKE THAT OUT
       fakeout = receive do
         after 10 ->
           true
         end
-      res = :ets.lookup(:packets, 'BOOM!')
+      res = :nil
       assert([{'BOOM!', 'BANG!'}] == res)
       # Clean up
-  #    :ok = Statx.Server.stop(server)
       :gen_udp.close(client_socket)
-
   end
 end

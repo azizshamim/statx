6b72870ea7fee8301de6416360f4635c9ed810fa
Aziz Shamim <azizshamim@gmail.com>
2015-03-17 03:16:25 -0500

    separate environements for logging

--- config/config.exs
+++ config/config.exs
@@ -9,11 +9,17 @@ use Mix.Config
 # party users, it should be done in your mix.exs file.
 
 # Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
+
+case System.get_env("MIX_ENV") do
+  "test" -> config :logger, :console,
+            level: :debug,
+            format: "$date $time [$level] $metadata$message\n",
+            metadata: [:user_id]
+  _ -> config :logger, :console,
+            level: :info,
+            format: "$date $time [$level] $metadata$message\n",
+            metadata: [:user_id]
+end
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
